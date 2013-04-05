07e68952fe207952e52477c3f7ccf86fe329c112
tacio <tcgtavares@gmail.com>
2013-04-05 17:12:34 -0300

    first test for the songs

---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+# import sys,os.path
+
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
---
+++ test/test_songs.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+
+from src.song import Song
+
+
+def config_working_path():
+    'Lets you call the tests from test folder or from project folder'
+    working_directory = os.getcwd()
+    folders = working_directory.split(os.sep)
+    if folders[-1] != 'test':
+        os.chdir(os.path.join(working_directory, 'test'))
+
+
+class Mp3Test(unittest.TestCase):
+    def test_file_not_found(self):
+        pass
+
+    def test_file_not_mp3(self):
+        pass
+
+    def test_mp3_constructor(self):
+        song = Song(os.path.join(working_directory, 'song-test.mp3'))
+        self.assertEqual(song.title, 'Skies')
+
+if __name__ == '__main__':
+    config_working_path()
+    working_directory = os.getcwd()
+    unittest.main()
