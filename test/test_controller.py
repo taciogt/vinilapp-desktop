b1ad91636b28a409e47d4594f15d090c66acc257
tacio <tcgtavares@gmail.com>
2013-04-15 20:18:16 -0300

    saving configuration for the library folder path

--- test/test_controller.py
+++ test/test_controller.py
@@ -2,13 +2,13 @@
 
 import unittest
 import os
-from vinilapp.controller import Controller
+from vinilapp.controller import Controller, Configuration, NoLibraryConfigured
 
 
 class ControllerTests(unittest.TestCase):
 
     def test_none_folder(self):
-        # check the behavior with a None music_folder
+        # check the behavior with a None music_folde
         pass
 
     def test_search_music_folder(self):
@@ -16,3 +16,21 @@ class ControllerTests(unittest.TestCase):
         test_folder = os.path.join(os.path.realpath(__file__), 'test-search-folder')
 
         controller.music_folder = test_folder
+
+
+config_file_test = 'test-configuration.cfg'
+
+
+class ConfigurationTests(unittest.TestCase):
+
+    def tearDown(self):
+        os.remove(config_file_test)
+
+    def test_creating_file(self):
+        self.assertRaises(TypeError, Configuration)
+
+        config = Configuration(config_file_test)
+        self.assertRaises(NoLibraryConfigured, config.get_library_path)
+
+        config.set_library_path('/home/music/')
+        self.assertEqual(config.get_library_path(), '/home/music/')
